cfc86c03a6c8c6a3b829bb06229417d21264a872
tomasalvaadison <tomas_alva_adison@teknokrat.ac.id>
2026-01-10 19:25:48 +0700

    Delete backend directory

--- backend/src/routes/quoteRoutes.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express';
-import { getRandomQuote, seedQuotes } from '../controllers/quoteController';
-
-const router = Router();
-
-router.get('/random', getRandomQuote);
-router.post('/seed', seedQuotes);
-
-export default router;
