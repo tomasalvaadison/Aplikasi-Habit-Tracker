f1ffbf2ae02e91fbb7ae450dd72401ddd497477a
tomasalvaadison <tomas_alva_adison@teknokrat.ac.id>
2026-01-15 20:45:06 +0700

    Update ScreenHeader.tsx

--- mobilee/src/components/ScreenHeader.tsx
+++ mobilee/src/components/ScreenHeader.tsx
@@ -1,3 +1,10 @@
 import React from 'react';
 import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+
+interface ScreenHeaderProps {
+  title: string;
+  showBackButton?: boolean;
+  onBackPress?: () => void;
+  rightComponent?: React.ReactNode;
+}
