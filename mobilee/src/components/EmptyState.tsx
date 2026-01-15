e1f9e2e51b2c14fb0f9d20a101d39531539e7d12
tomasalvaadison <tomas_alva_adison@teknokrat.ac.id>
2026-01-15 20:19:31 +0700

    Update EmptyState.tsx

--- mobilee/src/components/EmptyState.tsx
+++ mobilee/src/components/EmptyState.tsx
@@ -1,3 +1,11 @@
 import React from 'react';
 import { View, Text, StyleSheet, ViewStyle } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
+
+interface EmptyStateProps {
+  icon: keyof typeof Ionicons.glyphMap;
+  title: string;
+  subtitle?: string;
+  style?: ViewStyle;
+}
+
