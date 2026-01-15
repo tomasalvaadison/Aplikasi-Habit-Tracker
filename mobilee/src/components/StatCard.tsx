0c7088a6ad740201d89542f526656a66eff8843e
tomasalvaadison <tomas_alva_adison@teknokrat.ac.id>
2026-01-15 20:46:57 +0700

    Update StatCard.tsx

--- mobilee/src/components/StatCard.tsx
+++ mobilee/src/components/StatCard.tsx
@@ -2,3 +2,11 @@ import React from 'react';
 import { View, Text, StyleSheet, ViewStyle } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
+interface StatCardProps {
+  icon: keyof typeof Ionicons.glyphMap;
+  iconColor: string;
+  iconBackgroundColor?: string;
+  value: string | number;
+  label: string;
+  style?: ViewStyle;
+}
