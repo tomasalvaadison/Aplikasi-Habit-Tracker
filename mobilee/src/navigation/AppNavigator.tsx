675197a300fa0434bed6763687dca5a4765cfc67
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 20:15:37 +0700

    Update AppNavigator.tsx

--- mobilee/src/navigation/AppNavigator.tsx
+++ mobilee/src/navigation/AppNavigator.tsx
@@ -59,5 +59,5 @@ export default function AppNavigator() {
               name="Auth"
               component={AuthScreen}
               options={{ headerShown: false }}
-           
+            />
 
