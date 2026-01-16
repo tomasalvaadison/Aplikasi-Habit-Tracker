89897c758cf9d8b82dd5b30424935353ecff9281
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 19:20:02 +0700

    Update AppNavigator.tsx

--- mobilee/src/navigation/AppNavigator.tsx
+++ mobilee/src/navigation/AppNavigator.tsx
@@ -3,3 +3,5 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Ionicons } from  '@expo/vector-icons';
+
+import
