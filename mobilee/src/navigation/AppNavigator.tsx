87d25c33e70d427835782bf687b9d1037bf3b579
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 19:49:42 +0700

    Update AppNavigator.tsx

--- mobilee/src/navigation/AppNavigator.tsx
+++ mobilee/src/navigation/AppNavigator.tsx
@@ -27,3 +27,4 @@ function MainTabs() {
             } else if (route.name === 'Statistics') {
               iconName = focused ? 'stats-chart' : 'stats-chart-outline';
             } else if  (route.name === 'Profile') {
+              iconName
