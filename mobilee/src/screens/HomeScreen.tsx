2ea62f19fcb353fea471c7c874fe5feec6d6c176
allenaufal <alle_naufal_falih@teknokrat.ac.id>
2026-01-16 19:05:14 +0700

    Update HomeScreen.tsx

--- mobilee/src/screens/HomeScreen.tsx
+++ mobilee/src/screens/HomeScreen.tsx
@@ -22,3 +22,9 @@ import { habitAPI, logAPI, quoteAPI } from '../services/api';
 import { Habit, Quote } from '../types';
 import HabitCard from '../components/HabitCard';
 import EmptyState from '../components/EmptyState';
+
+  useFocusEffect(
+    useCallback(() => {
+      loadData();
+    }, [])
+  );
