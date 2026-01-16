ed2b79c41242427715d062ef01c7995445008aff
allenaufal <alle_naufal_falih@teknokrat.ac.id>
2026-01-16 19:02:48 +0700

    Update HomeScreen.tsx

--- mobilee/src/screens/HomeScreen.tsx
+++ mobilee/src/screens/HomeScreen.tsx
@@ -7,3 +7,10 @@ import {
   StyleSheet,
   RefreshControl,
   Alert,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { habitAPI, logAPI, quoteAPI } from '../services/api';
+import { Habit, Quote } from '../types';
+import HabitCard from '../components/HabitCard';
+import EmptyState from '../components/EmptyState';
