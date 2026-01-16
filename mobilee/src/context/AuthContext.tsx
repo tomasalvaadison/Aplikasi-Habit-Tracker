bfb1f4556eb26fb272445aa6fe55165c4ea137cb
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 18:50:28 +0700

    Update AuthContext.tsx

--- mobilee/src/context/AuthContext.tsx
+++ mobilee/src/context/AuthContext.tsx
@@ -1,3 +1,9 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { authAPI } from '../services/api';
+
+interface User {
+  id: number;
+  email: string;
+  name: string;
+}
