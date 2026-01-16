03d7a9f7f50a0cf540a8590c00c9800dff87abaf
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 19:56:45 +0700

    Update AppNavigator.tsx

--- mobilee/src/navigation/AppNavigator.tsx
+++ mobilee/src/navigation/AppNavigator.tsx
@@ -33,3 +33,4 @@ function MainTabs() {
              return <Ionicons name={iconName} size={size} color={color} />;
            },
            tabBarActiveTintColor: '#3B82F6'
+           tabBarInactiveTintColor:
