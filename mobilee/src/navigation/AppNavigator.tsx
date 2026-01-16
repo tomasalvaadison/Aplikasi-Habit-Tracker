41148446be95c85b23c6d5363a92c7c24b952794
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 19:53:12 +0700

    Update AppNavigator.tsx

--- mobilee/src/navigation/AppNavigator.tsx
+++ mobilee/src/navigation/AppNavigator.tsx
@@ -30,4 +30,4 @@ function MainTabs() {
               iconName = focused ? 'person' : 'person-outline';
             }
 
-             return <Ionicons name={iconName}
+             return <Ionicons name={iconName} size={size}
