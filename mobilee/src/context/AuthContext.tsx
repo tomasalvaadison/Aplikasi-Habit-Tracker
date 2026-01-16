cc6a45a84a659ffcb2079c15c9f90ff4833166ed
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 19:07:19 +0700

    Update AuthContext.tsx

--- mobilee/src/context/AuthContext.tsx
+++ mobilee/src/context/AuthContext.tsx
@@ -96,4 +96,7 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
 
 export const useAuth = () => {
     const context = useContext(AuthContext);
+  if (!context) {
+    throw new Error('useAuth must be used within AuthProvider');
+  }
 
