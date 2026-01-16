c7a315d19a9512465c2e03c34d9c6daa6ad640b8
allenaufal <alle_naufal_falih@teknokrat.ac.id>
2026-01-16 18:42:44 +0700

    Create AuthScreen.tsx

---
+++ mobilee/src/screens/AuthScreen.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Alert,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+} from 'react-native';
+import { useAuth } from '../context/AuthContext';
+import Input from '../components/Input';
+import Button from '../components/Button';
