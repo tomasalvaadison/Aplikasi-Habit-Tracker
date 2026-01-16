c6361609595e8ba63d8f852e09832468b1a7c4d7
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 18:58:54 +0700

    Update AuthContext.tsx

--- mobilee/src/context/AuthContext.tsx
+++ mobilee/src/context/AuthContext.tsx
@@ -67,3 +67,4 @@ export const AuthProvider: React.FC<{ children: React.ReactNode }> = ({ children
  const { user, token } = response.data;
 
       await AsyncStorage.setItem('token', token);
+      await AsyncStorage.setItem('user', JSON.stringify(user));
