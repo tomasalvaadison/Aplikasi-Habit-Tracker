5e02f481e98ced957e91914ef5d5fbb93e171ae9
allenaufal <alle_naufal_falih@teknokrat.ac.id>
2026-01-16 19:03:17 +0700

    Update HomeScreen.tsx

--- mobilee/src/screens/HomeScreen.tsx
+++ mobilee/src/screens/HomeScreen.tsx
@@ -14,3 +14,11 @@ import { habitAPI, logAPI, quoteAPI } from '../services/api';
 import { Habit, Quote } from '../types';
 import HabitCard from '../components/HabitCard';
 import EmptyState from '../components/EmptyState';
+
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
+import { habitAPI, logAPI, quoteAPI } from '../services/api';
+import { Habit, Quote } from '../types';
+import HabitCard from '../components/HabitCard';
+import EmptyState from '../components/EmptyState';
