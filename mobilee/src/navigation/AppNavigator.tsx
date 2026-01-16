0c674d6c1a2b94241c45e910fec0c0412ce8b8c6
alpine7337 <m_alvin_setiawan@teknokrat.ac.id>
2026-01-16 20:22:21 +0700

    Update AppNavigator.tsx

--- mobilee/src/navigation/AppNavigator.tsx
+++ mobilee/src/navigation/AppNavigator.tsx
@@ -70,5 +70,6 @@ export default function AppNavigator() {
               <Stack.Screen
                 name="AddHabit"
                 component={AddHabitScreen}
-               
+                options={{
+                 
 
